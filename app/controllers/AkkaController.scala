674a97e043d5095c633f5a02d2cde88c490006fa
Jeff <jeffwji@gmail.com>
2018-10-29 22:45:38 -0400

    Add akka controller

---
+++ app/controllers/AkkaController.scala
@@ -0,0 +1,36 @@
+package controllers
+
+import akka.actor.ActorSystem
+import javax.inject.Inject
+import play.api.mvc.{AbstractController, ControllerComponents}
+
+import scala.concurrent.{ExecutionContext, Future, Promise}
+import scala.concurrent.duration._
+
+
+/** 1) Play 集成了 Akka, 因此可以直接注入使用 */
+class AkkaController @Inject()(cc: ControllerComponents, actorSystem: ActorSystem)
+                              (implicit ec: ExecutionContext) // 无论是缺省的还是定制的都能被隐式捕获
+  extends AbstractController(cc) {
+
+  /** 2 - Akka async) 使用 akka 实现异步 */
+  def akkaAsync() = Action.async {
+    getAkkaFutureMessage(1.second).map { msg => Ok(msg) }
+  }
+
+  /** 3) 通过 akka actionSystem 获得未来任务 */
+  private def getAkkaFutureMessage(delayTime: FiniteDuration): Future[String] = {
+    val promise: Promise[String] = Promise[String]()
+
+    // 通过 ActionSystem 分发
+    actorSystem.scheduler.scheduleOnce(delayTime) {
+      // 定义 promise
+      promise.success {
+        "Hi! Akka!!"
+      }
+    }(actorSystem.dispatcher) // run scheduled tasks using the actor system's dispatcher
+
+    // 触发 promise，返回 Future
+    promise.future
+  }
+}
--- app/controllers/AsyncController.scala
+++ app/controllers/AsyncController.scala
@@ -2,10 +2,11 @@ package controllers
 
 // 参考：http://www.voidcn.com/article/p-mmsxwcuu-bqy.html
 
+import akka.actor.ActorSystem
 import javax.inject.Inject
 import play.api.mvc.{AbstractController, ControllerComponents}
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ExecutionContext, Future, Promise}
 
 /** 1）在 application.conf 中配置了一个 Akka 线程池，它会被当作缺省的线程池引入。
   *
@@ -13,21 +14,24 @@ import scala.concurrent.{ExecutionContext, Future}
   *   class MyExecutionContext @Inject()(actorSystem: ActorSystem) extends CustomExecutionContext(actorSystem, "my-context")
   *
   * */
-class AsyncController @Inject()(implicit ec: ExecutionContext, // 无论是缺省的还是定制的都能被隐式捕获
-                                cc: ControllerComponents) extends AbstractController(cc) {
+class AsyncController @Inject()(cc: ControllerComponents)
+                               (implicit ec: ExecutionContext) // 无论是缺省的还是定制的都能被隐式捕获
+                               extends AbstractController(cc) {
 
-  /** 2）定义异步任务函数，这个函数返回一个 Future，并被 controller 的入口方法调用。 */
-  def futureJob()= {
-    Future {
-      "Yes"
-    }
-  }
 
-  /** 3）使用 Action.async 方法，该方法接受一个 Future 参数来异步执行任务 */
+  /** 2 - Normal future）使用 Action.async 方法，该方法接受一个 Future 参数来异步执行任务 */
   def async() = Action.async {
     val asyncApiResponse: Future[String] = futureJob()
 
     /** 4）将异步任务的结果映射成 Response */
     asyncApiResponse.map(value => Ok("Api Result: " + value))
   }
+
+  /** 3）定义异步任务函数，这个函数返回一个 Future，并被 controller 的入口方法调用。 */
+  private def futureJob()= {
+    Future {
+      "Yes"
+    }
+  }
+
 }
